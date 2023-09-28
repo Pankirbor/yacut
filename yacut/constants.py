116543c6f80d0e208069c91f447a156661d3d858
Кирилл Панов <lliril_05@list.ru>
2023-09-29 01:53:37 +0300

    Исправлена обработка ошибок в шаблоне yacut.html, добавлена валидация по регулярному выражению в формах, добавлены константы для forms.py

--- yacut/constants.py
+++ yacut/constants.py
@@ -3,8 +3,10 @@ from string import ascii_letters, digits
 HOST = "http://localhost/"
 SIMBOLS = digits + ascii_letters
 PATTERN_VALID_SHORT_NAME = r"[\W\sА-я]+"
+PATTERN_REGEXP_VALIDATOR = r"[^\W\sА-я\s]+$"
 MAX_LENGTH_SHORT_NAME = 16
-MIN_LENGTH_SHORT_NAME = 1
+MAX_LENGTH = 256
+MIN_LENGTH = 1
 KEYS_DATA = ("url", "custom_id")
 KEYS_TO_DICT = ("url", "short_link")
 MODEL_FIELDS = dict(url="original", custom_id="short")
@@ -16,3 +18,7 @@ URL_REQUIRED_MSG = '"url" является обязательным полем!'
 DUPLICATE_NAME_MSG = "Имя {short} уже занято{punctuation}"
 ACCEPT = "Ссылка успешно создана ;)"
 NOT_FOUND = "Указанный id не найден"
+HELP_TEXT_ORIGINAL_LINK = "Введите оригинальную ссылку."
+HELP_TEXT_SHORT_LINK = "Ваш вариант короткой ссылки."
+ERROR_REQUIRED_FIELD = "Это поле обязательно к заполнению."
+SUBMIT_BUTTON = "Создать"
--- yacut/forms.py
+++ yacut/forms.py
@@ -1,16 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import URLField, SubmitField
-from wtforms.validators import DataRequired, Optional, Length
+from wtforms.validators import DataRequired, Optional, Length, Regexp
+
+from yacut.constants import (
+    ERROR_REQUIRED_FIELD,
+    HELP_TEXT_ORIGINAL_LINK,
+    HELP_TEXT_SHORT_LINK,
+    INVALID_NAME_MSG,
+    MAX_LENGTH,
+    MAX_LENGTH_SHORT_NAME,
+    MIN_LENGTH,
+    PATTERN_REGEXP_VALIDATOR,
+    SUBMIT_BUTTON,
+)
 
 
 class URLForm(FlaskForm):
     """Класс описания формы с валидацией данных."""
 
     original_link = URLField(
-        "Введите оригинальную ссылку",
-        validators=[DataRequired(message="Обязательное поле"), Length(1, 256)],
+        HELP_TEXT_ORIGINAL_LINK,
+        validators=[
+            DataRequired(message=ERROR_REQUIRED_FIELD),
+            Length(MIN_LENGTH, MAX_LENGTH),
+        ],
     )
     custom_id = URLField(
-        "Ваш вариант короткой ссылки", validators=[Optional(), Length(1, 16)]
+        HELP_TEXT_SHORT_LINK,
+        validators=[
+            Optional(),
+            Length(MIN_LENGTH, MAX_LENGTH_SHORT_NAME),
+            Regexp(PATTERN_REGEXP_VALIDATOR, message=INVALID_NAME_MSG),
+        ],
     )
-    submit = SubmitField("Создать")
+    submit = SubmitField(SUBMIT_BUTTON)
